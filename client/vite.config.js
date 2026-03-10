0620873912ea110cb9cb415128131268e2bb6f2e
ZoldyckX <tabibhassan.1604@gmail.com>
2026-03-10 19:24:47 +0600

    Frontend Structure completed

---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // Add Tailwind CSS plugin
+  ],
+  server: {
+    port: 5173,
+    open: true
+  }
+})
